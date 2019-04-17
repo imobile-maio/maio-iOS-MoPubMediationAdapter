e8d30419627a15d9dfd83aa278cf88f48efa9aa9
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2019-04-17 19:37:56 +0900

    Add MaioAdapterConfiguration
    skelton.

---
+++ objc/mopub.ObjectiveC/mopub.ObjectiveC/MaioMoPubAdapter/MaioAdapterConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  MaioAdapterConfiguration.h
+//  mopub.ObjectiveC
+//
+//  Created by Kasamatsu on 2019/04/17.
+//  Copyright © 2019 maio. All rights reserved.
+//
+
+#import "MPBaseAdapterConfiguration.h"
+
+@interface MaioAdapterConfiguration : MPBaseAdapterConfiguration
+
+@end
---
+++ objc/mopub.ObjectiveC/mopub.ObjectiveC/MaioMoPubAdapter/MaioAdapterConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  MaioAdapterConfiguration.m
+//  mopub.ObjectiveC
+//
+//  Created by Kasamatsu on 2019/04/17.
+//  Copyright © 2019 maio. All rights reserved.
+//
+
+#import "MaioAdapterConfiguration.h"
+
+@implementation MaioAdapterConfiguration
+
+@end
