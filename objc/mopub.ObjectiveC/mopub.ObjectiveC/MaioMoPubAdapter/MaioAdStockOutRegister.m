e043a4f339c552adaf631aec825d4f98408a785a
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2019-05-30 19:48:58 +0900

    Add class: MaioAdStockOutRegister
    skeleton

---
+++ objc/mopub.ObjectiveC/mopub.ObjectiveC/MaioMoPubAdapter/MaioAdStockOutRegister.m
@@ -0,0 +1,17 @@
+//
+//  MaioAdStockOutRegister.m
+//  mopub.ObjectiveC
+//
+//  Created by Kasamatsu on 2019/05/30.
+//  Copyright © 2019 maio. All rights reserved.
+//
+
+#import "MaioAdStockOutRegister.h"
+
+@implementation MaioAdStockOutRegister
+
+- (BOOL)hasRecordThatZoneId:(NSString *)zoneId {
+    return NO;
+}
+
+@end
