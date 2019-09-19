eea83a3d8884bdda6fcbcbf48c6dd554a78ee183
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2019-09-19 19:51:32 +0900

    Edit script: archive adapter

--- bin/archive.sh
+++ bin/archive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 readonly DOC_ROOT=$(cd $(dirname ${BASH_SOURCE:-$0})/.. ; pwd)
 readonly ADAPTER_ROOT="$DOC_ROOT/objc/mopub.ObjectiveC/mopub.ObjectiveC/MaioMoPubAdapter"
+readonly ADAPTER_NAME='MaioMoPubAdapter'
 
 # echo $DOC_ROOT 1>&2
 # echo $ADAPTER_ROOT 1>&2
@@ -28,3 +29,18 @@ do
 done
 
 # echo $destination 1>&2
+
+# drop argument of option
+shift $(($OPTIND - 1))
+
+if [ $# -eq 0 ]; then
+    echo "argument not found." 1>&2
+    show_help
+    exit 1
+fi
+
+for commit in "$@"; do
+    cd $DOC_ROOT;
+    git checkout $commit
+    zip -r $destination/"${ADAPTER_NAME}_${commit}.zip" $ADAPTER_ROOT
+done
